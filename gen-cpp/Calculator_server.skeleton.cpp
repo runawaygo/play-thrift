adcce6a8635a43afece975929e6e74afe41083fd
superwolf.runaway <superwolf.runaway@gmail.com>
2013-09-04 23:21:36 +0800

    ...

--- gen-cpp/Calculator_server.skeleton.cpp
+++ gen-cpp/Calculator_server.skeleton.cpp
@@ -16,54 +16,47 @@ using boost::shared_ptr;
 
 using namespace  ::tutorial;
 
-class CalculatorHandler : virtual public CalculatorIf {
+class CalculatorHandler : public CalculatorIf {
  public:
-  CalculatorHandler() {
-    // Your initialization goes here
-  }
+  CalculatorHandler() {}
 
   void ping() {
-    // Your implementation goes here
-    printf("ping\n");
+    printf("ping()\n");
   }
 
-  int32_t add(const int32_t num1, const int32_t num2) {
-    // Your implementation goes here
-    printf("add\n");
+  int32_t add(const int32_t n1, const int32_t n2) {
+    printf("add(%d,%d)\n", n1, n2);
     return n1 + n2;
   }
 
-  int32_t calculate(const int32_t logid, const Work& w) {
-    // Your implementation goes here
+  int32_t calculate(const int32_t logid, const Work &work) {
+    printf("calculate(%d,{%d,%d,%d})\n", logid, work.op, work.num1, work.num2);
     int32_t val;
 
-    printf("calculate\n");
-    printf("calculate(%d,{%d,%d,%d})\n", logid, work.op, work.num1, work.num2);
-    
     switch (work.op) {
-      case Operation::ADD:
-        val = work.num1 + work.num2;
-        break;
-      case Operation::SUBTRACT:
-        val = work.num1 - work.num2;
-        break;
-      case Operation::MULTIPLY:
-        val = work.num1 * work.num2;
-        break;
-      case Operation::DIVIDE:
-        if (work.num2 == 0) {
-          InvalidOperation io;
-          io.what = work.op;
-          io.why = "Cannot divide by 0";
-          throw io;
-        }
-        val = work.num1 / work.num2;
-        break;
-      default:
+    case Operation::ADD:
+      val = work.num1 + work.num2;
+      break;
+    case Operation::SUBTRACT:
+      val = work.num1 - work.num2;
+      break;
+    case Operation::MULTIPLY:
+      val = work.num1 * work.num2;
+      break;
+    case Operation::DIVIDE:
+      if (work.num2 == 0) {
         InvalidOperation io;
         io.what = work.op;
-        io.why = "Invalid Operation";
+        io.why = "Cannot divide by 0";
         throw io;
+      }
+      val = work.num1 / work.num2;
+      break;
+    default:
+      InvalidOperation io;
+      io.what = work.op;
+      io.why = "Invalid Operation";
+      throw io;
     }
 
     SharedStruct ss;
@@ -82,13 +75,12 @@ class CalculatorHandler : virtual public CalculatorIf {
     ret = log[logid];
   }
 
-
   void zip() {
-    // Your implementation goes here
-    printf("zip\n");
+    printf("zip()\n");
   }
-  protected:
-    map log;
+
+protected:
+  map log;
 
 };
 
